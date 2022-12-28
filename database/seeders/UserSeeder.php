3b006daf7b93d5b230c3589aed0efeaaf2b0dd6a
Yudi Saeful Rizal <yudienam666@gmail.com>
2022-12-28 21:07:18 +0700

    1
    1

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,7 +15,7 @@ class UserSeeder extends Seeder
     public function run()
     {
 
-         $user = User::create([
+        $user = User::create([
             'name'      => 'Kepala Sekolah',
             'username'  => 'kepsek',
             'email'     => 'kepsek@sch.id',
@@ -26,7 +26,48 @@ class UserSeeder extends Seeder
 
         $user->assignRole('Admin');
 
-        $this->command->info('Data User '.$user->name.' has been saved.');
-       
+        $this->command->info('Data User ' . $user->name . ' has been saved.');
+
+        $user = User::create([
+            'name'      => 'PPDB',
+            'username'  => 'PPDB',
+            'email'     => 'PPDB@sch.id',
+            'role'      => 'PPDB',
+            'status'    => 'Aktif',
+            'password'  => bcrypt('Bismillah')
+        ]);
+
+        $user->assignRole('PPDB');
+
+        $this->command->info('Data User ' . $user->name . ' has been saved.');
+
+
+        $user = User::create([
+            'name'      => 'staf',
+            'username'  => 'bendahara',
+            'email'     => 'staf@sch.id',
+            'role'      => 'Staf',
+            'status'    => 'Aktif',
+            'password'  => bcrypt('Bismillah')
+        ]);
+
+        $user->assignRole('Staf');
+
+        $this->command->info('Data User ' . $user->name . ' has been saved.');
+
+
+
+        $user = User::create([
+            'name'      => 'murid',
+            'username'  => 'murid',
+            'email'     => 'murid@sch.id',
+            'role'      => 'Murid',
+            'status'    => 'Aktif',
+            'password'  => bcrypt('Bismillah')
+        ]);
+
+        $user->assignRole('Murid');
+
+        $this->command->info('Data User ' . $user->name . ' has been saved.');
     }
 }
